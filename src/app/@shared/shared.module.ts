edbdf72301a5406f43dc32df966e60311b1e3028
Ivan D. Velez <ivancho175@hotmail.com>
2022-08-14 17:21:43 -0500

    build: add header navigation  - add navigation in order to navigate between forms and modules

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
+
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatMenuModule, MatRippleModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
